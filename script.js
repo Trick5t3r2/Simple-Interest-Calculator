59378a3c780042856eb2060bbd101df6f1ce9098
Trick5t3r2 <89535429+Trick5t3r2@users.noreply.github.com>
2021-09-01 17:56:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;  
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit " + "<mark>" + principal + "</mark>."  + ", <br> at an interest rate of "+ "<mark>" + rate + "</mark>%." + "<br> You will receive an amount of " + "<mark>" + interest + "</mark>" + ", <br> in the year " + "<mark>" + yearNow + "</mark>";
     
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
